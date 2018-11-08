f7caf34848bdb83ac83a2eb69bdef886fd813b2d
loleeta <lolitalam3@gmail.com>
2018-11-08 13:11:21 -0800

    general scan actually works

--- src/com/lolita/GeneralScan.java
+++ src/com/lolita/GeneralScan.java
@@ -1,112 +1,103 @@
 package com.lolita;
 
 import java.util.ArrayList;
-import java.lang.Math;
 import java.util.List;
-import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.RecursiveAction;
 
+public class GeneralScan<ElemType, TallyType> {
 
-public class GeneralScan<ElemType, TallyType extends Tally<ElemType>> {
-    public static final int NUM_THREADS = 16;
-
+    private static final int NUM_THREADS = 16;
     private int ROOT = 0;
+    private List<ElemType> data;
+    private List<TallyType> interior;
+    private int n;
     private boolean reduced;
-    private int n; //size of data, n-1 is size of interior
-    private ArrayList<ElemType> data;
-    private ArrayList<TallyType> interior;
-    private TallyType tallyFactory;
     private int height;
 
-    //public methods
-    public GeneralScan(ArrayList<ElemType> data, TallyType factory) {
-        this.reduced = false;
-        this.n = data.size();
+    public GeneralScan(List<ElemType> data) {
         this.data = data;
-        this.height = (int)Math.ceil(Math.log(n)/Math.log(2));
-        if (1<<height != n)
-            throw new java.lang.RuntimeException("Data size must be pwoer of 2");
+        this.n = data.size();
+        reduced = false;
+        this.height = (int) Math.ceil(Math.log(n)/Math.log(2));
+
+        if (1 << height != n) {
+            System.out.println("Not a power of 2.  Exiting");
+            System.exit(1);
+        }
         this.interior = new ArrayList<TallyType>(n-1);
-        for (int i = 0; i < (n-1); i++)
-            interior.set(i, init());
-        this.tallyFactory = factory;
+
+        for (int i = 0; i < n-1; i++) {
+            interior.add(init());
+        }
     }
 
-    public TallyType getReduction(Integer i) {
-        //return getReduction(0)
-        Integer param = i != null ? i : 0;
-        this.reduced = reduced || reduce(ROOT);
-        return value(param);
-    }
-
-    public void getScan(ArrayList<TallyType> output) {
-        reduced = reduced || reduce(ROOT);
-        scan(ROOT, init(), output);
-    }
-
-    //protected methods
-    //throw an exception newTallyT
     protected TallyType init() {
-        return (TallyType) tallyFactory.init();
-    }
-
-    protected TallyType prepare(ElemType datum) {
-        return (TallyType) tallyFactory.prepare(datum);
+        throw new IllegalArgumentException("Have to finish this part");
     }
 
     protected TallyType combine(TallyType left, TallyType right) {
-       left.combine(right);
-       return left;
+        throw new IllegalArgumentException("Have to finish this part");
     }
 
-    //private methods
-    private int size() {
-        return (n-1) + n;
+    protected TallyType prepare(ElemType datum) {
+        throw new IllegalArgumentException("Have to finish this part");
     }
 
-    private TallyType value(int i) {
-        if (i < n-1)
-            return interior.get(i);
-        else
-            return prepare(data.get(i-(n-1)));
+    protected void accum(TallyType tally, ElemType datum) {
+        throw new IllegalArgumentException("Have to finish this part");
     }
 
-    private int parent(int i) {
-        return (i-1)/2;
+    TallyType getReduction() {
+        reduced = reduced || reduce(ROOT);
+        return value(ROOT);
     }
 
-    private int left(int i) {
-        return i*2+1;
-    }
 
-    private int right(int i) {
-        return left(i)+1;
-    }
-
-    private boolean isLeaf(int i) {
-        return right(i) >= size();
-    }
-
-    private boolean reduce(int i) {
+    boolean reduce(int i) {
         if (!isLeaf(i)) {
-            if (i < NUM_THREADS -2) {
+            if (i < NUM_THREADS-2) {
+                reduce(left(i));
+                reduce(right(i));
+            } else {
                 reduce(left(i));
                 reduce(right(i));
             }
-            interior.set(i, combine(value(left(i)), value(right(i))));
+
+            TallyType tally = combine(value(left(i)), value(right(i)));
+            interior.set(i,tally);
         }
         return true;
     }
 
-    private void scan(int i, TallyType tallyPrior, ArrayList<TallyType> output) {
-        if (isLeaf(i)) {
-            output.set(i-(n-1), combine(tallyPrior, value(i)));
+    TallyType value(int i) {
+        TallyType t = init();
+        if (i < n-1) {
+            return interior.get(i);
         } else {
-            if (i < NUM_THREADS-2) {
-                scan(left(i), tallyPrior, output);
-                scan(right(i), combine(tallyPrior, value(left(i))), output);
-            }
+            accum(t, data.get(i - (n-1)));
+            return t;
         }
     }
 
+    int size() {
+        return (n-1) + n;
+    }
+
+    int parent(int i) {
+        return (i-1)/2;
+    }
+
+    int left(int i) {
+        return (i*2)+1;
+    }
+
+    int right(int i) {
+        return (i*2) + 2;
+    }
+
+    boolean isLeaf(int i) {
+        if(i >= n-1)
+            return true;
+        else
+            return false;
+    }
 }
